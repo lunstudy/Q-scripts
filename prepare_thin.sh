a35b3b936511857921a17efffd2e4880519f4c96
lunstudy <lunstudy@gmail.com>
2025-12-04 15:48:41 +0800

    Add files via upload

---
+++ prepare_thin.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+wget https://www.dropbox.com/s/597akae23dpzshq/screen.sh
+wget http://download.qnap.com/Storage/tsd/utility/tp_collect.sh
+sh screen.sh
+
+mount_path=$(df -h | awk '$6 ~ /^\/share\/external\/DEV/ {print $6}' | head -n 1)
+
+
+if [ -z "$mount_path" ]; then
+    echo "Can't find any USB HDD"
+    exit 1
+fi
+
+echo "Find the USB HDD, USB HDD path: $mount_path"
+echo
+
+if grep -q "^COLLECT=" tp_collect.sh; then
+    sed -i "s|^COLLECT=.*|COLLECT=$mount_path|" tp_collect.sh
+else
+    echo "COLLECT=\"$mount_path\"" >> tp_collect.sh
+fi
+
+echo "Already add USB HDD path into the tp_collect.sh file"
+echo
